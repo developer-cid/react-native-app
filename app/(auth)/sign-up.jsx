a388d87f42e0641cb443204523360b5c491ca2a0
Sid <hiimcidz@gmail.com>
2024-06-06 22:42:58 +0800

    This is a tutorial  coming from JS MASTERY

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, router } from "expo-router";
+
+// Constants
+import images from "../../constants/images";
+
+// Components
+import FormField from "../components/FormField";
+import CustomButton from "../components/CustomButton";
+
+// Library
+import { createUser } from "../../lib/appwrite";
+
+// Context
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+const SignUp = () => {
+  const { setIsLoggedIn, setUser } = useGlobalContext();
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form?.email || !form?.password || !form?.username) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      // Set created user to user context
+      setUser(result);
+      setIsLoggedIn(true);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="justify-center w-full min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl text-white text-semibol mt-10 font-psemibold">
+            Sign up to Aora
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-adddress"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
---
+++ app/components/EmptyState.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { router } from "expo-router";
+
+// Components
+import CustomButton from "./CustomButton";
+
+// Constants
+import { images } from "../../constants";
+
+const EmptyState = ({ title, subtitle }) => {
+  return (
+    <View className="justify-center items-center px-4">
+      <Image
+        source={images.empty}
+        resizeMode="contain"
+        className="w-[270px] h-[215px]"
+      />
+      <Text className="text-xl text-center font-psemibold text-white mt-2">
+        {title}
+      </Text>
+      <Text className="font-pmedium text-sm text-gray-100">{subtitle}</Text>
+
+      <CustomButton
+        title="Create Video"
+        handlePress={() => router.push("/create")}
+        containerStyles="w-full my-5"
+      />
+    </View>
+  );
+};
+
+export default EmptyState;
